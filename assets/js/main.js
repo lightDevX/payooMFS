13ae9d81196f1f40eab16382751da0075dcc84aa
lightdevx <khan.m.hasanuzzaman@gmail.com>
2025-03-01 09:34:49 +0600

    dashbord create

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,13 @@
 document.getElementById('btn-login')
     .addEventListener('click', function (e) {
         e.preventDefault();
-        console.log('Button clicked');
-
         const phoneNumber = document.getElementById('phone-number').value;
-        console.log(phoneNumber);
+        const pinNumber = document.getElementById('pin').value;
 
+        if (phoneNumber === '01713788801' && pinNumber === '1234') {
+            window.location.href = '/dashboard.html';
+        } else {
+            alert('Invalid phone number or pin');
+        }
 
     });
